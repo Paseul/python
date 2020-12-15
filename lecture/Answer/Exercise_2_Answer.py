b5f2fdd97ad895ff44a90f2302cf94f532a97cc1
Paseul <machianb@naver.com>
2020-12-16 08:07:20 +0900

    Create Exercise_2_Answer.py

---
+++ lecture/Answer/Exercise_2_Answer.py
@@ -0,0 +1,30 @@
+# YOUR CODE SHOULD START HERE
+# YOUR CODE SHOULD END HERE
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# YOUR CODE SHOULD START HERE
+class myCallbacks(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy')>0.99):
+            print("Reached 99% accuracy so cancelling training!")
+            self.model.stop_training=True
+
+callbacks = myCallbacks()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+# YOUR CODE SHOULD END HERE
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(256, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# YOUR CODE SHOULD START HERE
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+# YOUR CODE SHOULD END HERE
