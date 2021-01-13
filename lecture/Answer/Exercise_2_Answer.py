e9c628d6360f435144b3f4db90c865e170085463
Jaehoon Lee <machianb@naver.com>
2021-01-13 15:47:42 +0900

    Update Exercise_2_Answer.py

--- lecture/Answer/Exercise_2_Answer.py
+++ lecture/Answer/Exercise_2_Answer.py
@@ -1,19 +1,21 @@
-# YOUR CODE SHOULD START HERE
-# YOUR CODE SHOULD END HERE
 import tensorflow as tf
+import numpy as np
 
-mnist = tf.keras.datasets.mnist
+# mnist = tf.keras.datasets.mnist
+#
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-# YOUR CODE SHOULD START HERE
+path = tf.keras.utils.get_file('mnist.npz', 'dataset/mnist.npz')
+data = np.load(path)
+
 class myCallbacks(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         if(logs.get('accuracy')>0.99):
-            print("Reached 99% accuracy so cancelling training!")
+            print("\nReached 99% accuracy so cancelling training!")
             self.model.stop_training=True
 
 callbacks = myCallbacks()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train, x_test = data['x_train'] / 255.0, data['x_test'] / 255.0
 # YOUR CODE SHOULD END HERE
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -26,5 +28,5 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # YOUR CODE SHOULD START HERE
-model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+model.fit(x_train, data['y_train'], epochs=10, callbacks=[callbacks])
 # YOUR CODE SHOULD END HERE
