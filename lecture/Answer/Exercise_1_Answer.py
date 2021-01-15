57508953c8ac7c5fca1ddccb9b6f1bebc152e199
Paseul <machianb@naver.com>
2021-01-15 11:40:34 +0900

    Update Exercise_1_Answer.py

--- lecture/Answer/Exercise_1_Answer.py
+++ lecture/Answer/Exercise_1_Answer.py
@@ -10,4 +10,10 @@ model.compile(
 xs = np.array([1, 2, 3, 4, 5])
 ys = np.array([100, 150, 200, 250, 300])
 model.fit(xs, ys, epochs=500)
+
 print(model.predict([7.0]))
+
+model.save('exercise1.h5')
+# It can be used to reconstruct the model identically.
+reconstructed_model = keras.models.load_model("exercise1.h5")
+print(reconstructed_model.predict([7.0]))
